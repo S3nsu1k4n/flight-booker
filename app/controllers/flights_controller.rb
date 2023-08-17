8379e44a65b1f9e6004e83ea720b39391385d715
Sascha Koeppe <saschakoeppe@gmail.com>
2023-08-17 13:32:06 +0900

    Add dropdown menus to search in flight#index

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,4 +1,7 @@
 class FlightsController < ApplicationController
   def index
+    @airport_options = Airport.all.map { |a| [a.code, a.id] }
+    @flight_dates = Flight.all.map { |f| [f.departure_date.strftime('%Y/%m/%d')] }.uniq.sort
+    @flight = Flight.all
   end
 end
