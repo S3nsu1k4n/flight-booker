c761ef6e55b3de3c476138a1f218ebe6a390dc40
Sascha Koeppe <saschakoeppe@gmail.com>
2023-08-17 14:39:18 +0900

    add available flights to flight#index

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,5 +3,14 @@ class FlightsController < ApplicationController
     @airport_options = Airport.all.map { |a| [a.code, a.id] }
     @flight_dates = Flight.all.map { |f| [f.departure_date.strftime('%Y/%m/%d')] }.uniq.sort
     @flight = Flight.all
+    @available_flights = Flight.where(flight_params) if params.key? :flight
+    p @available_flights
   end
+
+  private
+
+  def flight_params
+    params.require(:flight).permit(:departure_airport_id, :arrival_airport_id, :departure_date)
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,19 +9,19 @@
 Airport.destroy_all
 Flight.destroy_all
 
-20.times do
+10.times do
   Airport.create(code: SecureRandom.send(:choose, [*'A'..'Z'], 3))
 end
 
 p "Created #{Airport.count} airport codes"
 
 airport_ids = Airport.ids
-20.times do
+500.times do
   departure_id = SecureRandom.send(:choose, airport_ids, 1).to_i
   arrival_id = SecureRandom.send(:choose, airport_ids - [departure_id], 1).to_i
-  date = Date.today + SecureRandom.random_number(31).days
-  duration_hours = SecureRandom.random_number(24) + 1
-  Flight.create(departure_airport_id:departure_id, arrival_airport_id:arrival_id, departure_date:date, duration:duration_hours)
+  date = Date.today + SecureRandom.random_number(7).days
+  duration_hours = SecureRandom.random_number(23) + 1
+  Flight.create(departure_airport_id:departure_id, arrival_airport_id: arrival_id, departure_date: date, duration: duration_hours)
 end
 
 p "Created #{Flight.count} flights"
