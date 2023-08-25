b8a21c7e93f4d0a76b31ff81fbb24de86113ced5
Sascha Koeppe <saschakoeppe@gmail.com>
2023-08-25 10:17:31 +0900

    create mails for passenger

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,9 @@
 class PassengerMailer < ApplicationMailer
+  default from: 'notification@example.com'
+
+  def welcome_email
+    @passenger = params[:passenger]
+    @url = 'http://example.com/login'
+    mail(to: @passenger.email, subject: 'Welcome to my awesome site')
+  end
 end
